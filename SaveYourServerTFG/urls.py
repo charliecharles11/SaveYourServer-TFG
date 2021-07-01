d28e7b7c2650cdfd790e3a66d4a74fc72d0f6bc8
charliecharles11 <79619880+charliecharles11@users.noreply.github.com>
2021-07-01 22:25:01 +0200

    Add files via upload

---
+++ SaveYourServerTFG/urls.py
@@ -0,0 +1,30 @@
+"""SaveYourServerTFG URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+#from SaveYourServerTFGApp.admin import system_admin_site
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('SaveYourServerTFGApp.urls')),
+    #path('', include('SaveYourServerTFGApp.urls')),
+]
+
+admin.site.site_header = "Save Your Server Admin"
+admin.site.site_title = "SYS Portal"
+admin.site.index_title = "Welcome to the SYS Researcher Portal"
+
